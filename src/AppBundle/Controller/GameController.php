c1bb5d727d8309866fe121a87d258fba9b630dee
HeahDude <heah@heahprod.com>
2017-11-08 10:18:25 +0100

    Added support for launching a new game

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -2,17 +2,36 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Game\GameContext;
+use AppBundle\Game\GameRunner;
+use AppBundle\Game\Loader\TextFileLoader;
+use AppBundle\Game\Loader\XmlFileLoader;
+use AppBundle\Game\WordList;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class GameController extends Controller
 {
     /**
      * @Route("/game", name="game_index")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('game/index.html.twig');
+        $wordList = new WordList();
+        $wordList->addLoader('txt', new TextFileLoader());
+        $wordList->addLoader('xml', new XmlFileLoader());
+        $wordList->loadDictionaries([
+            __DIR__.'/../../../app/Resources/data/test.txt',
+            __DIR__.'/../../../app/Resources/data/words.txt',
+            __DIR__.'/../../../app/Resources/data/words.xml',
+        ]);
+
+        $runner = new GameRunner(new GameContext($request->getSession()), $wordList);
+
+        return $this->render('game/index.html.twig', [
+            'game' => $runner->loadGame(),
+        ]);
     }
 
     /**
